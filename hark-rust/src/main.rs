9720963569abb9ea20b66181dfcac4a1026042d1
Srivathsav-Kyatham <2023.srivathsavk@isu.ac.in>
2025-08-17 20:16:29 +0530

    first push

---
+++ hark-rust/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let is_male = false;
+    let is_above_18 = true;
+
+    if is_male{
+        println!("You are a male.");
+    } else {
+        println!("You are not a male.");
+    }
+    if is_male && is_above_18 {
+        println!("You are a legal male.");
+    }
+}
